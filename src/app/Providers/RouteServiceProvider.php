d95ff053d64a95c02e32ba4a880d5ae02fe1e54f
quantm <quantm.tb@gmail.com>
2023-03-05 13:54:01 +0700

    Fix route bugs

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -38,11 +38,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-         /*   Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));*/
-
+            Route::get('/', 'HomeController@index');
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->domain(config('domain.manager'))
--- src/routes/customer.php
+++ src/routes/customer.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'HomeController@index');
+
 
 /**
  * Main customer routes
